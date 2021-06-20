dffa1452502d3bcf5630c3ae9f028090f7b961a7
Nihal Chari <30363800+nihalchari@users.noreply.github.com>
2021-06-20 17:02:17 +0530

    Update check_ifor_sorted_array.c

--- 20_06/check_ifor_sorted_array.c
+++ 20_06/check_ifor_sorted_array.c
@@ -13,6 +13,19 @@ int is_sorted(int *arr, int elements)
         }
         
     }
+    
+    ///@note alternate logic
+    /*
+        for(int i = 0; i<elements-1; ++i)
+    {
+        if(arr[i] > arr[i+1])
+        {
+            return -1;
+        }
+        
+    }
+    */
+    
     return 0;
     
 }
