4f3dcebfcbbb23fc0e41bf415ae2e00b7cc9e473
Nihal Chari <30363800+nihalchari@users.noreply.github.com>
2021-07-01 17:30:43 +0530

    added add_at_position_before

--- linked_list.c
+++ linked_list.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct node_type
 {
     int data;
@@ -126,10 +129,71 @@ void delete_front(node **head, node **tail)
     return;
 }
 
+int add_at_value(int val, int pos, node **head, node **tail)
+{
+    if(*head == NULL  && *tail  == NULL)
+    {
+        printf("\nList is empty\n\n");
+        return -1;
+    }
+    
+    node * newnode = (node *)malloc(sizeof(node));
+    if(newnode == NULL)
+    {
+        printf("add node :Memory not available\n");
+        return -1;
+    }
+    newnode->data = val;
+    newnode->next = NULL;
+    
+    if((*head == *tail) )
+    {
+        if((*head)-> data == pos)
+        {    newnode->next = *head;
+            *head = newnode;
+            return 0;
+        }
+        else{
+            printf("value at position not found\n");
+            return -1;
+        }
+    }
+
+    if((*head)->data == pos)
+    {
+        newnode->next = *head;
+        *head = newnode;
+        return 0;
+    }
+    
+    node * trev = (*head);
+    node *prev =(*head)->next;
+    
+    
+    while(trev->next != NULL && trev->data != pos)
+    {
+        prev = trev;
+        trev = trev->next;
+    }
+    
+    if(trev->data == pos)
+    {
+        newnode->next = prev->next;
+        prev->next = newnode;
+        return 0;
+    }
+    else{
+         printf("value at position not found\n");
+            return -1;
+    }
+    
+    return -1;
+}
+
 
 int main()
 {
-    int choice=0, val=0;
+    int choice=0, val=0, pos=0;
     node *head =NULL;
     node *tail =NULL;
   
@@ -137,7 +201,7 @@ int main()
     while(1)
     {
         printf("Enter Choice\n");
-        printf("1. Add node at start\n2. Add node at end\n3. Delete node at start\n4. Delete node at end\n5. Display List\n0. Exit\n") ;
+        printf("1. Add node at start\n2. Add node at end\n3. Delete node at start\n4. Delete node at end\n5. Display List\n6. Add_at_value\n0. Exit\n") ;
         scanf("%d",&choice);
         
         switch(choice)
@@ -166,6 +230,14 @@ int main()
             
             case 5:
             display_list(&head);
+            break;
+            
+            case 6:
+            printf("Enter the value\n");
+            scanf("%d",&val);
+            printf("Enter the value where you want to add\n");
+            scanf("%d",&pos);
+            add_at_value(val, pos, &head, &tail);
             break;
             
             case 0:
